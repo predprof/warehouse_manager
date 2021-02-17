0e80fcacbf2a4e240b4c1ea7ad6d2d3e2c3a4c8c
Dmitry Morozov <flatura@gmail.com>
2021-02-17 19:04:58 +0300

    Оптимизация схемы БД. Добавление функции сохранения

--- db.py
+++ db.py
@@ -1,9 +1,8 @@
 from sqlalchemy import MetaData, create_engine, Integer, Column, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import sessionmaker
 
 metadata = MetaData()
-# engine = create_engine('postgresql://admin:1234@localhost:5432/storage_warehouse_olymp', connect_args={'check_same_thread': False}, echo=False)
 engine = create_engine('postgresql://admin:12345@localhost:5432/storage_manager_olymp', echo=True)
 Base = declarative_base()
 db_session = sessionmaker(bind=engine)()
@@ -12,14 +11,12 @@ db_session = sessionmaker(bind=engine)()
 class Stowage(Base):
     __tablename__ = 'stowages'
     id = Column(Integer, primary_key=True)
-    pos_x = Column(Integer)
-    pos_y = Column(Integer)
+    name = Column(String)
     size_x = Column(Integer)
     size_y = Column(Integer)
     size_z = Column(Integer)
-    volume = Column(Integer)
-    weight = Column(Integer)
     status = Column(String)
+    item_id = Column(ForeignKey('items.id'))
 
 
 class Item(Base):
@@ -29,11 +26,21 @@ class Item(Base):
     size_x = Column(Integer)
     size_y = Column(Integer)
     size_z = Column(Integer)
-    volume = Column(Integer)
     weight = Column(Integer)
     status = Column(String)
-    stowage_id = Column(ForeignKey('stowages.id'))
 
 
+# Получить список товаров
 def get_items():
     return db_session.query(Item)
+
+
+# Получить список ячеек
+def get_stowages():
+    return db_session.query(Stowage)
+
+
+# Сохранить ячейку
+def add_stowage(new_stowage):
+    db_session.add(new_stowage)
+    db_session.commit()
